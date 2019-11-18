3bba5469d49a4d3300658fb6761eac7d66b12d90
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-18 17:10:31 -0600

    extend message from data so as to get objectId and createdAt properities from Parse

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -9,8 +9,6 @@ var FormView = {
     // Stop the browser from submitting the form
     event.preventDefault();
 
-    console.log('click the submit button!');
-
     var message = {
       username: App.username,
       // Get the value of the #message id in $form
@@ -18,12 +16,14 @@ var FormView = {
       roomname: 'lobby',
     };
 
-    Parse.create(message, () => {
+    Parse.create(message, (data) => {
+      // Parse doesn't return the entire object, it just returned the properities it created(objectId and createdAt)). So here we used _.extend() to add Parse sepcific properties(like objectId and createdAt) with the message we created
+      // _.extend(destination, *sources) is used to shallowly copy all of the properties in the source objects over to the destination object, and return the destination object
+      _.extend(message, data);
       // If we pass the message to the Parse server and create it successfully, we will go and re-render the messages
       // Since MessagesView has nothing to do with fetching the data, so we will need to concat the newly created message to Messages
       // Make MessagesView.render as a callback and refresh the displayed messages if the new message was added successfully
       Messages.add(message, MessagesView.render);
-      console.log('chatterbox: Message Sent!');
     });
   },
 
