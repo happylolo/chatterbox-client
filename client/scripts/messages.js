bb2bc231074250e07683499972c2c29ef1ee4523
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-17 22:44:11 -0600

    Used _.chain(obj) to hide _data in Messages from MessagesView

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -2,8 +2,13 @@ var Messages = {
   // underbar here is just a notice to the developer: don't use the data directly
   _data: {},
 
+  items: function () {
+    // _.chain(obj) returns a wrapped object. Calling methods on this object will continue to return wrapped objects until "value" is called
+    return _.chain(Messages._data);
+  },
+
   // () => {} means the default callback, so that this function always has a callback
-  add: function (message, callback = () => { }) {
+  add: function (message, callback = () => {}) {
     Messages._data[message.objectId] = Messages._conform(message);
 
     callback();
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -4,13 +4,18 @@ var MessagesView = {
   initialize: function() {
   },
 
-  render: function() {
-    _.each(Messages._data, (message) => {
-      var $message = MessageView.render(message);
-      // append to the end
-      // MessagesView.$chats.append($message);
-      // prepend to the top
-      MessagesView.$chats.prepend($message);
-    });
+  // In order not to expose Messages._data to MessagesView, we can use a Messages.item() which is a chainable object
+  render: function () {
+    // We can debug in the chrome by adding a dubugger here like this if we want:
+    // debugger;
+    Messages
+      .items()
+      // .filter(Room.isSelected(message))
+      .each((message) => {
+        var $message = MessageView.render(message);
+        // Append to the end: MessagesView.$chats.append($message);
+        // Prepend to the top:
+        MessagesView.$chats.prepend($message);
+      });
   },
 };
